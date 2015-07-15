5c68ea1cdabc9548653bdce97126db1ffe0d6c62
Abelino Lobato González <abelino.lobato@live.itsz.edu.mx>
2015-07-15 14:35:59 -0500

    Integración Sequalize y SQlite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,23 @@ exports.answer = function (req,res) {
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
+
+var models = require('../models/models.js');
+
+//GET /quizes/question
+exports.question = function (req, res) {
+	models.Quiz.findAll().suceess(function(quiz) {
+			res.render('quizes/question', { pregunta: quiz[0].pregunta});
+	})
+};
+
+//GET /quizes/answer
+exports.answer = function (req, res) {
+	models.Quiz.findAll().suceess(function(quiz) {
+		if(req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', { respuesta: 'Correcto'});
+		}else {
+			res.render('quizes/answer', { respuesta: 'Incorrecto'});
+		}
+	})
+};
