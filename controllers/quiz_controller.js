df5a6522bad42022fdad8cf7f4c9cb42240e7123
Abelino Lobato González <abelino.lobato@live.itsz.edu.mx>
2015-07-28 18:09:02 -0500

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -36,6 +36,24 @@ exports.show = function (req, res) {
 	res.render('quizes/show', { quiz: req.quiz});
 };
 
+
+// GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build({ pregunta: "Pregunta", respuesta: "Respuesta" });
+	res.render('quizes/new', { quiz: quiz });
+};
+
+
+// POST /quizes/create
+exports.create = function (req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	//guarda en la BD los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function() {
+		res.redirect('/quizes');
+	}) //Redirección HTTP (URL relativo) lista de preguntas
+};
+
+
 //GET /quizes/answer
 exports.answer = function (req, res) {
 	var resultado = 'Incorrecto';
